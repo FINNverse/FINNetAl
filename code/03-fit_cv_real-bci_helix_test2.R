f35148936b199434009b090ded8a62630bcd2740
ykaeber <y.kaeber@posteo.de>
2025-03-06 00:02:49 +0100

    fix

--- code/03-fit_cv_real-bci_helix_test2.R
+++ code/03-fit_cv_real-bci_helix_test2.R
@@ -39,7 +39,7 @@ for(i_dir in directories){
 #   library(glmmTMB)
 # })
 
-# i_var = all_variants[[1]]
+i_var = all_variants[[1]]
 # remove all objects in the environment but "overwrite" and "i_var"
 
 # rm(list = setdiff(ls(), c("overwrite", "all_lossvars", "Nepochs", "i_var")))
@@ -143,7 +143,7 @@ for(i_dir in directories){
 
     cat("\nfit model:", i_name)
     m1$fit(data = obs_dt, batchsize = batchsize, env = env_dt, init_cohort = cohort1,  epochs = Nepochs, patches = Npatches, lr = 0.01, checkpoints = 5L,
-           optimizer = torch::optim_ignite_adam, device = "gpu", record_gradients = FALSE, weights = c(0.1, 10, 1.0, 1, 1, 1), plot_progress = FALSE,
+           optimizer = torch::optim_ignite_adam, device = "cpu", record_gradients = FALSE, weights = c(0.1, 10, 1.0, 1, 1, 1), plot_progress = FALSE,
            loss= c(dbh = "mse", ba = "mse", trees = "nbinom", growth = "mse", mortality = "mse", regeneration = "nbinom")
     )
 
