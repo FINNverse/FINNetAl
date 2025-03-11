fc6609acdbe709fe1097651e9753ffa1fb080f80
ykaeber <y.kaeber@posteo.de>
2025-03-11 12:39:58 +0100

    Update 03-fit_cv_real_hybrid-bci_helix.R

--- code/03-fit_cv_real_hybrid-bci_helix.R
+++ code/03-fit_cv_real_hybrid-bci_helix.R
@@ -215,7 +215,7 @@ parallel::clusterExport(cl, varlist = c(ls(envir = .GlobalEnv)), envir = environ
     Npatches = uniqueN(cohorts_dt$patchID)
 
     m1$fit(data = obs_dt, batchsize = batchsize, env = env_dt, init_cohort = cohort1,  epochs = Nepochs, patches = Npatches, lr = 0.01, checkpoints = 5L,
-           optimizer = torch::optim_ignite_adam, device = "cpu", record_gradients = FALSE,weights = c(0.1, 10, 1.0, 10.0, 1, 1), plot_progress = FALSE,
+           optimizer = torch::optim_ignite_adam, device = "gpu", record_gradients = FALSE,weights = c(0.1, 10, 1.0, 10.0, 1, 1), plot_progress = FALSE,
            loss= c(dbh = "mse", ba = "mse", trees = "nbinom", growth = "mse", mortality = "mse", regeneration = "nbinom")
     )
 
